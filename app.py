78c1c01d32138e6964941bf3b92b23596c5d0466
jhendricks <jhendricks@consultwithcase.com>
2022-07-26 14:52:54 -0400

    change article structure. style page

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
-import os
-import spacy
+import spacy, os, datetime, requests, pickle
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
@@ -14,7 +12,7 @@ first = True
 def index():
     global first
     if first:
-        createArticles()
+        apiCall()
         createIndexes()
         first = False
     return loadTemplate()
@@ -32,19 +30,28 @@ def loadTemplate():
     return render_template("index.html", data=data, searchTerm=searchTerm)
 
 
-def createArticles():
-    """
-    Reads in the database results and turns them into individual files
-    """
-    f = open('test.json', 'r')
-    jsonString = json.loads(f.read())
-    articleArray = jsonString['response']['results']
-    for article in articleArray:
-        id = article['id'].replace('/', '_')
-        newF = open(f"./articles/{id}.txt", "w")
-        newF.write(article['fields']['bodyText'])
-        newF.close()
-    f.close()
+def apiCall():
+    """Connects to The Guardian api and retrieves articles from the past week."""
+    today = datetime.date.today()
+    from_date = today - datetime.timedelta(days=7)
+    toady_string = today.strftime('%Y-%m-%d')
+    from_string = from_date.strftime('%Y-%m-%d')
+    page = 1
+    added = True
+    artObj = {}
+    articles = open("articles.p", "ab")
+    while added:
+        url = f'http://content.guardianapis.com/search?page={page}&page-size=100&from-date={from_string}&to-date={toady_string}&order-by=newest&show-fields=bodyText&api-key=test'
+        jsonString = requests.get(url).json()
+        if 'results' in jsonString['response']:
+            articleArray = jsonString['response']['results']
+            for article in articleArray:
+                artObj[article['id']] = article['fields']['bodyText']
+            page+=1
+        else:
+            added = False
+    pickle.dump(artObj, articles)
+    articles.close()
 
 
 def createIndexes():
@@ -52,40 +59,39 @@ def createIndexes():
     Reads each article file and generates indexes for each doc, entity, and frequency.
     """
     nlp = spacy.load("en_core_web_sm")  # will use english model
-    index = open('index.json', "w")
+    index = open('index.p', "ab")
     indexes = {
         'doc_lookup': {},
         'entity_lookup': {},
         'frequency_lookup': {},
     }
     count = 0
-
-    for filename in os.listdir(f'{os.getcwd()}/articles'):
+    articles = open('articles.p', 'rb')
+    full_file = pickle.load(articles)
+    for file in full_file:
         # add doc indexes
-        indexes['doc_lookup'][count] = filename
-        # open in readonly mode
-        with open(os.path.join(f'{os.getcwd()}/articles', filename), 'r') as f:
-            doc = nlp(f.read())
-            filtered_tokens = [preprocess_token(token) for token in doc if is_token_allowed(token)]
-            for token in filtered_tokens:
+        indexes['doc_lookup'][count] = file
+        doc = nlp(full_file[file])
+        # filtered_tokens = [preprocess_token(token) for token in doc if is_token_allowed(token)]
+        for token in doc:# filtered_tokens:
+            if is_token_allowed(token):
+                token = preprocess_token(token)
                 # add entity and frequency indexes
                 if token in indexes['entity_lookup']:
                     if count not in indexes['entity_lookup'][token]:
-                        indexes['entity_lookup'][token].append(
-                            count)
+                        indexes['entity_lookup'][token].append(count)
                         indexes['frequency_lookup'][token] += 1
                 else:
                     indexes['entity_lookup'][token] = [count]
                     indexes['frequency_lookup'][token] = 1
         count += 1
-    export = json.dumps(indexes)
-    index.write(export)
+    pickle.dump(indexes, index)
     index.close()
 
 
 def is_token_allowed(token):
      """Only allow valid tokens which are not stop words and punctuation symbols."""
-     if (not token or not token.text.strip() or token.is_stop or token.is_punct):
+     if (token.is_stop or token.is_punct):
          return False
      return True
 
@@ -97,25 +103,28 @@ def preprocess_token(token):
 
 def search(term):
     """Given a search term, lookup the entity in the index and get the article files that contain the search."""
-    f = open('index.json', 'r')
-    j = json.loads(f.read())
+    f = open('index.p', 'rb')
+    j = pickle.load(f)
     docs = []
     main_term = getLowestFreq(term, j)
-    print(main_term)
-    if(main_term):
+    if main_term in j['entity_lookup']:
         for doc in j['entity_lookup'][main_term]:
-            path = (j['doc_lookup'][f'{doc}'])[:-4].replace('_', '/')
+            path = (j['doc_lookup'][doc])
             docs.append(
                 f'https://www.theguardian.com/{path}')
     return docs
 
 
 def getLowestFreq(term, j):
-    terms = term.split(' ')
+    """Reduces the search term to the lowest frequency word. Helpful to improve exact-match searches."""
+    nlp = spacy.load("en_core_web_sm")  # will use english model
+    terms = nlp(term)
     lowest_count = 10000
     lowest_term = None
     for term in terms:
-        if term in j['frequency_lookup'] and j['frequency_lookup'][term] < lowest_count:
-            lowest_count = j['frequency_lookup'][term]
-            lowest_term = term
+        if is_token_allowed(term):
+            term = preprocess_token(term)
+            if term in j['frequency_lookup'] and j['frequency_lookup'][term] < lowest_count:
+                lowest_count = j['frequency_lookup'][term]
+                lowest_term = term
     return lowest_term
--- main.py
+++ main.py
@@ -1,62 +1,71 @@
-import json
-import spacy
-import os
+import json, spacy, os, datetime, requests, pickle, time
 
-
-def createArticles():
-    """
-    Reads in the database results and turns them into individual files
-    """
-    f = open('test.json', 'r')
-    jsonString = json.loads(f.read())
-    articleArray = jsonString['response']['results']
-    for article in articleArray:
-        id = article['id'].replace('/', '_')
-        newF = open(f"./articles/{id}.txt", "w")
-        newF.write(article['fields']['bodyText'])
-        newF.close()
-    f.close()
+def apiCall():
+    """Connects to The Guardian api and retrieves articles from the past week."""
+    today = datetime.date.today()
+    from_date = today - datetime.timedelta(days=7)
+    toady_string = today.strftime('%Y-%m-%d')
+    from_string = from_date.strftime('%Y-%m-%d')
+    page = 1
+    added = True
+    artObj = {}
+    articles = open("articles.p", "ab")
+    while added:
+        url = f'http://content.guardianapis.com/search?page={page}&page-size=10&from-date={from_string}&to-date={toady_string}&order-by=newest&show-fields=bodyText&api-key=test'
+        jsonString = requests.get(url).json()
+        if 'results' in jsonString['response']:
+            articleArray = jsonString['response']['results']
+            for article in articleArray:
+                artObj[article['id']] = article['fields']['bodyText']
+            page+=1
+            added=False
+        else:
+            added = False
+    pickle.dump(artObj, articles)
+    articles.close()
+    print('finished saving ', page)
 
 
 def createIndexes():
     """
     Reads each article file and generates indexes for each doc, entity, and frequency.
     """
+    start = time.time()
     nlp = spacy.load("en_core_web_sm")  # will use english model
-    index = open('index.json', "w")
+    index = open('index.p', "ab")
     indexes = {
         'doc_lookup': {},
         'entity_lookup': {},
         'frequency_lookup': {},
     }
     count = 0
-
-    for filename in os.listdir(f'{os.getcwd()}/articles'):
+    articles = open('articles.p', 'rb')
+    full_file = pickle.load(articles)
+    for file in full_file:
         # add doc indexes
-        indexes['doc_lookup'][count] = filename
-        # open in readonly mode
-        with open(os.path.join(f'{os.getcwd()}/articles', filename), 'r') as f:
-            doc = nlp(f.read())
-            filtered_tokens = [preprocess_token(token) for token in doc if is_token_allowed(token)]
-            for token in filtered_tokens:
-                # add entity and frequency indexes
-                if token in indexes['entity_lookup']:
-                    if count not in indexes['entity_lookup'][token]:
-                        indexes['entity_lookup'][token].append(
-                            count)
-                        indexes['frequency_lookup'][token] += 1
-                else:
-                    indexes['entity_lookup'][token] = [count]
-                    indexes['frequency_lookup'][token] = 1
+        indexes['doc_lookup'][count] = file
+        doc = nlp(full_file[file])
+        for entity in doc.ents:
+            # add entity and frequency indexes
+            entity = entity.text.strip().lower()
+            if entity in indexes['entity_lookup']:
+                if count not in indexes['entity_lookup'][entity]:
+                    indexes['entity_lookup'][entity].append(count)
+                    indexes['frequency_lookup'][entity] += 1
+            else:
+                indexes['entity_lookup'][entity] = [count]
+                indexes['frequency_lookup'][entity] = 1
         count += 1
-    export = json.dumps(indexes)
-    index.write(export)
+    pickle.dump(indexes, index)
     index.close()
+    end = time.time()
+    print(end-start)
+
 
 
 def is_token_allowed(token):
      """Only allow valid tokens which are not stop words and punctuation symbols."""
-     if (not token or not token.text.strip() or token.is_stop or token.is_punct):
+     if (token.is_stop or token.is_punct):
          return False
      return True
 
@@ -68,29 +77,37 @@ def preprocess_token(token):
 
 def search(term):
     """Given a search term, lookup the entity in the index and get the article files that contain the search."""
-    f = open('index.json', 'r')
-    j = json.loads(f.read())
+    f = open('index.p', 'rb')
+    j = pickle.load(f)
     docs = []
     main_term = getLowestFreq(term, j)
-    for doc in j['entity_lookup'][main_term]:
-        path = (j['doc_lookup'][f'{doc}'])[:-4].replace('_', '/')
-        docs.append(
-            f'https://www.theguardian.com/{path}')
+    if main_term in j['entity_lookup']:
+        for doc in j['entity_lookup'][main_term]:
+            path = (j['doc_lookup'][doc])
+            docs.append(
+                f'https://www.theguardian.com/{path}')
     return docs
 
 
 def getLowestFreq(term, j):
-    terms = term.split(' ')
+    """Reduces the search term to the lowest frequency word. Helpful to improve exact-match searches."""
+    nlp = spacy.load("en_core_web_sm")  # will use english model
+    terms = nlp(term)
     lowest_count = 10000
     lowest_term = None
     for term in terms:
-        if term in j['frequency_lookup'] and j['frequency_lookup'][term] < lowest_count:
-            lowest_count = j['frequency_lookup'][term]
-            lowest_term = term
+        if is_token_allowed(term):
+            term = preprocess_token(term)
+            if term in j['frequency_lookup'] and j['frequency_lookup'][term] < lowest_count:
+                lowest_count = j['frequency_lookup'][term]
+                lowest_term = term
     return lowest_term
 
 
 if __name__ == "__main__":
-    #createArticles()
-    #createIndexes()
-    search('uk ran')
+    apiCall()
+    createIndexes()
+    f = open('index.p', 'rb')
+    j = pickle.load(f)
+    for key in j['entity_lookup']:
+        if ' ' in key: print(key)
