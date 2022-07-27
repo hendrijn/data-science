a1755b7b74a293b39a3b2bb64ef1c252306bcc23
jhendricks <jhendricks@consultwithcase.com>
2022-07-27 09:09:06 -0400

    finish downloading and indexing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json, spacy, os, datetime, requests, pickle, time
+from operator import index
 
 def apiCall():
     """Connects to The Guardian api and retrieves articles from the past week."""
@@ -9,21 +10,20 @@ def apiCall():
     page = 1
     added = True
     artObj = {}
-    articles = open("articles.p", "ab")
+    articles = open("articles.p", "wb")
     while added:
-        url = f'http://content.guardianapis.com/search?page={page}&page-size=10&from-date={from_string}&to-date={toady_string}&order-by=newest&show-fields=bodyText&api-key=test'
+        url = f'http://content.guardianapis.com/search?page={page}&page-size=100&from-date={from_string}&to-date={toady_string}&order-by=newest&show-fields=bodyText&api-key=test'
         jsonString = requests.get(url).json()
         if 'results' in jsonString['response']:
             articleArray = jsonString['response']['results']
             for article in articleArray:
                 artObj[article['id']] = article['fields']['bodyText']
             page+=1
-            added=False
         else:
             added = False
     pickle.dump(artObj, articles)
     articles.close()
-    print('finished saving ', page)
+    print('finished saving ', len(artObj))
 
 
 def createIndexes():
@@ -32,30 +32,29 @@ def createIndexes():
     """
     start = time.time()
     nlp = spacy.load("en_core_web_sm")  # will use english model
-    index = open('index.p', "ab")
+    index = open('index.p', "wb")
     indexes = {
-        'doc_lookup': {},
-        'entity_lookup': {},
-        'frequency_lookup': {},
+        'doc_lookup':{},
+        'entity_lookup':{},
+        'frequency_lookup':{}
     }
-    count = 0
+    doc_id = 0
     articles = open('articles.p', 'rb')
     full_file = pickle.load(articles)
     for file in full_file:
         # add doc indexes
-        indexes['doc_lookup'][count] = file
+        indexes['doc_lookup'][doc_id] = file
         doc = nlp(full_file[file])
         for entity in doc.ents:
             # add entity and frequency indexes
             entity = entity.text.strip().lower()
-            if entity in indexes['entity_lookup']:
-                if count not in indexes['entity_lookup'][entity]:
-                    indexes['entity_lookup'][entity].append(count)
-                    indexes['frequency_lookup'][entity] += 1
+            if entity in indexes['entity_lookup'] and doc_id not in indexes['entity_lookup'][entity]:
+                indexes['entity_lookup'][entity].add(doc_id)
+                indexes['frequency_lookup'][entity] += 1
             else:
-                indexes['entity_lookup'][entity] = [count]
+                indexes['entity_lookup'][entity] = {doc_id}
                 indexes['frequency_lookup'][entity] = 1
-        count += 1
+        doc_id += 1
     pickle.dump(indexes, index)
     index.close()
     end = time.time()
@@ -63,16 +62,16 @@ def createIndexes():
 
 
 
-def is_token_allowed(token):
-     """Only allow valid tokens which are not stop words and punctuation symbols."""
-     if (token.is_stop or token.is_punct):
-         return False
-     return True
+# def is_token_allowed(token):
+#      """Only allow valid tokens which are not stop words and punctuation symbols."""
+#      if (token.is_stop or token.is_punct):
+#          return False
+#      return True
 
 
-def preprocess_token(token):
-    # Reduce token to its lowercase lemma form
-    return token.lemma_.strip().lower()
+# def preprocess_token(token):
+#     # Reduce token to its lowercase lemma form
+#     return token.lemma_.strip().lower()
 
 
 def search(term):
@@ -105,9 +104,10 @@ def getLowestFreq(term, j):
 
 
 if __name__ == "__main__":
-    apiCall()
-    createIndexes()
+    # apiCall()
+    # createIndexes()
     f = open('index.p', 'rb')
     j = pickle.load(f)
+    # print(j['entity_lookup'])
     for key in j['entity_lookup']:
-        if ' ' in key: print(key)
+        if len(j['entity_lookup'][key]) > 3: print(key, end=' . ')
